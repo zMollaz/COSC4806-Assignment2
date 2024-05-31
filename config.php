d1835ca2d8b9417da5d5ab1d3186860f9d3cb947
Ahmed Elmolla <87554450+zMollaz@users.noreply.github.com>
2024-05-31 04:15:44 +0000

    Added online database config and connection

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST',         '8o4.h.filess.io');
+define('DB_USER',         'COSC4806_porchlotat'); 
+define('DB_PASS',         $_ENV['DB_PASS']);
+define('DB_DATABASE',     'COSC4806_porchlotat');
+define('DB_PORT',         '3305');
+?>
---
+++ database.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'database.php';
+
+function db_connect() {
+    try { 
+        $dbh = new PDO('mysql:host=' . DB_HOST . ';port='. DB_PORT . ';dbname=' . DB_DATABASE, DB_USER, DB_PASS);
+        return $dbh;
+    } catch (PDOException $e) {
+        //We should set a global variable here so we know the DB is down
+    }
+}
+?>
